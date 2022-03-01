fb647f1136f2d0327f80f4c0471e46035483dbbd
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-03-01 16:35:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
 
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
@@ -6,9 +8,15 @@ function getRandomQuote() {
      .then(data => data.content)
 }
 
-async function getNextQuote() {
+async function renderNewQuote() {
   const quote = await getRandomQuote()
-  console.log(quote)
+  quoteDisplayElement.innerText = quote
+  quote.split('').forEach(character => {
+      const characterSpan = document.createElement('span')
+      characterSpan.innerText = character
+      quoteDisplayElement.appendChild(characterSpan)
+  })
+  quoteInputElement.value = null
 }
 
-getNextQuote()
+renderNewQuote()
